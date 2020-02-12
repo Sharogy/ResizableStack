d5072d4a93ab55126116fc9e473e9f82182cdf0c
Shar <Shar@DESKTOP-BNPA2DR.home>
2020-02-12 04:16:39 +0100

    test

---
+++ src/ResizableStack.java
@@ -0,0 +1,114 @@
+import java.lang.reflect.Array;
+
+public class ResizableStack<Item> {
+	private Item[] stack;
+	private int top;
+	private int stacksize;
+	private Class<?> c;
+	@SuppressWarnings("unchecked")
+	public ResizableStack (int length, Item... cls){
+		if (cls.length > 0)
+			throw new IllegalArgumentException("Do not provide values for class argument.");
+		c = cls.getClass().getComponentType();
+		stack = (Item[]) Array.newInstance(c, length);
+		top = 0;
+		stacksize = length;
+	}
+		
+	@SuppressWarnings("unchecked")
+	public void push(Item item)
+	{
+		if (top+1 > stacksize/2)
+		{
+			Item[] newstack = (Item[]) Array.newInstance(c, stacksize*2);
+			for (int i = 0; i<top; i++)
+			{
+				newstack[i] = stack[i];
+			}
+			stack = newstack;
+			stacksize = stacksize*2;
+		}
+	
+		stack[top++] = item;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public Item pop()
+	{
+		if (isEmpty())
+		{
+			System.out.println("empty");
+			return null;
+		}
+		else
+		{
+			if (top < Math.round(stacksize/2))
+			{
+				Item[] newstack = (Item[]) Array.newInstance(c, Math.round(stacksize/2));
+				for (int i = 0; i<top; i++)
+				{
+					newstack[i] = stack[i];
+				}
+				stack = newstack;
+				stacksize = stacksize/2;
+			}
+			top--;
+			return stack[top];
+		}		
+	}
+	
+	public boolean isEmpty()
+	{	
+		return top == 0;
+	}
+	
+	public int size()
+	{
+		return top;
+	}
+	
+	@Override
+    public String toString() {
+        return "Resizablestack of " + stack.getClass().getComponentType().getName()
+            + "[" + stack.length + "]";
+    }
+	
+	public static void main (String[] argz)
+	{
+		ResizableStack<Integer> teststack = new ResizableStack<Integer>(4);
+		
+		teststack.push(2);
+		teststack.push(3);
+		teststack.push(4);
+		
+		System.out.println("testing stack 1, commencing");
+		System.out.println();
+		System.out.println(teststack);
+		System.out.println(teststack.size());
+		System.out.println(teststack.isEmpty());
+
+		int i = teststack.pop();
+		int j = teststack.pop();
+		int k = teststack.pop();
+		System.out.println(teststack);
+		System.out.println(i);
+		System.out.println(j);
+		System.out.println(k);
+		System.out.println(teststack.size());
+		System.out.println(teststack.isEmpty());
+		teststack.pop();	
+		
+		ResizableStack<Object> teststack2 = new ResizableStack<Object>(4);
+		teststack2.push("Test");
+		teststack2.push(5);
+		teststack2.push(5.0);
+		System.out.println();
+		System.out.println("testing stack 2, commencing");
+		System.out.println();
+		System.out.println(teststack2.pop());
+		System.out.println(teststack2.pop());
+		System.out.println(teststack2.pop());
+		
+
+	}
+}
---
+++ src/Stackviewer.java
@@ -0,0 +1,134 @@
+
+public class Stackviewer<T> {
+	ResizableStack<T> thisstack;
+	
+	public Stackviewer(ResizableStack<T> s)
+	{
+		thisstack = s;
+	}
+	
+	public T viewtopitem()
+	{
+		T t = thisstack.pop();
+		thisstack.push(t);
+		return t;
+	}
+	
+	public void removetopitem()
+	{
+		thisstack.pop();
+		
+	}
+	
+	@SuppressWarnings("unchecked")
+	public T viewbottomitem()
+	{
+		T t = null;
+		ResizableStack<T> s2 = new ResizableStack<T>(thisstack.size());
+		while (!thisstack.isEmpty())
+		{
+			t = thisstack.pop();
+			s2.push(t);
+		}
+		int tempsize = s2.size();
+		for (int i = 0; i<tempsize; i++)
+		{
+			thisstack.push(s2.pop());
+		}	
+		return t;
+	}
+	
+
+	@SuppressWarnings("unchecked")
+	public void removebottomitem()
+	{
+		ResizableStack<T> s2 = new ResizableStack<T>(thisstack.size());
+		ResizableStack<T> s3 = new ResizableStack<T>(thisstack.size());
+		int tempsize = thisstack.size();
+		for (int i = 1; i < tempsize; i++)
+		{
+			s2.push(thisstack.pop());
+		}
+		int tempsize2 = s2.size();
+		for (int i = 0; i < tempsize2; i++)
+		{
+			s3.push(s2.pop());
+		}
+		thisstack = s3;	
+	}
+	
+	@SuppressWarnings("unchecked")
+	public T viewnthitem(int index)
+	{
+		ResizableStack<T> s2 = new ResizableStack<T>(thisstack.size());
+		if (index <= thisstack.size())
+		{
+			T t = null;
+			for (int i = 0; i<index; i++)
+			{
+				t = thisstack.pop();
+				s2.push(t);
+			}
+			int tempsize = s2.size();
+			for (int i = 0; i<tempsize; i++)
+			{
+				thisstack.push(s2.pop());
+			}
+			return t;
+		}
+		else
+			return null;
+	}
+
+	@SuppressWarnings("unchecked")
+	public void removenthitem(int index)
+	{
+		if (index <= thisstack.size())
+		{
+			ResizableStack<T> s2 = new ResizableStack<T>(thisstack.size());
+			for (int i = 0; i < index; i ++ )
+			{
+				s2.push(thisstack.pop());				
+			}
+			s2.pop();
+			int tempsize = s2.size();
+			for (int i = 0; i<tempsize; i++)
+			{
+				thisstack.push(s2.pop());
+			}
+		}
+		else
+		{
+			System.out.println("No such element");
+		}
+			
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static void main (String[] argz)
+	{
+		ResizableStack<Object> rs = new ResizableStack<>(4);
+		rs.push("test");
+		rs.push(5);
+		rs.push(4.0);
+		rs.push("test2");
+		rs.push(10);
+		rs.push(6.6);
+		
+		Stackviewer sv = new Stackviewer(rs);
+		
+		System.out.println(sv.viewnthitem(4));
+		System.out.println(sv.viewtopitem());
+		sv.removetopitem();
+		System.out.println(sv.viewtopitem());
+		System.out.println(sv.viewbottomitem());
+		sv.removebottomitem();
+		System.out.println(sv.viewbottomitem());
+		sv.removenthitem(5);
+		System.out.println(sv.viewnthitem(3));
+		sv.removenthitem(3);
+		System.out.println(sv.viewnthitem(3));
+		
+	}
+
+}
